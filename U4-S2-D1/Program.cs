ba9ad2672a282fdf3e0ced9e579b08415d5b2a0e
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-17 17:27:13 +0100

    Adds new version

--- U4-S2-D1/Program.cs
+++ U4-S2-D1/Program.cs
@@ -15,65 +15,63 @@ namespace U4_S2_D1
             double conto = 0;
             string input;
 
-            do
+        Start:
+            Console.WriteLine("==============MENU==============" +
+            "\r\n\r\n1:  Coca Cola 150 ml (€ 2.50)" +
+            "\r\n\r\n2:  Insalata di pollo (€ 5.20)" +
+            "\r\n\r\n3:  Pizza Margherita (€ 10.00)" +
+            "\r\n\r\n4:  Pizza 4 Formaggi (€ 12.50)" +
+            "\r\n\r\n5:  Pz patatine fritte (€ 3.50)" +
+            "\r\n\r\n6:  Insalata di riso (€ 8.00)" +
+            "\r\n\r\n7:  Frutta di stagione (€ 5.00)" +
+            "\r\n\r\n8:  Pizza fritta (€ 5.00)" +
+            "\r\n\r\n9:  Piadina vegetariana (€ 6.00)" +
+            "\r\n\r\n10: Panino Hamburger (€ 7.90)" +
+            "\r\n\r\n11: Stampa conto finale e conferma" +
+            "\r\n\r\n==============MENU==============");
+
+            Console.WriteLine("\r\nSelezionare il prodotto dal menù o digitare 11 per la stampa del conto:\r\n");
+            input = Console.ReadLine();
+
+            switch (input)
             {
-                Console.WriteLine("==============MENU==============" +
-                "\r\n\r\n1:  Coca Cola 150 ml (€ 2.50)" +
-                "\r\n\r\n2:  Insalata di pollo (€ 5.20)" +
-                "\r\n\r\n3:  Pizza Margherita (€ 10.00)" +
-                "\r\n\r\n4:  Pizza 4 Formaggi (€ 12.50)" +
-                "\r\n\r\n5:  Pz patatine fritte (€ 3.50)" +
-                "\r\n\r\n6:  Insalata di riso (€ 8.00)" +
-                "\r\n\r\n7:  Frutta di stagione (€ 5.00)" +
-                "\r\n\r\n8:  Pizza fritta (€ 5.00)" +
-                "\r\n\r\n9:  Piadina vegetariana (€ 6.00)" +
-                "\r\n\r\n10: Panino Hamburger (€ 7.90)" +
-                "\r\n\r\n11: Stampa conto finale e conferma" +
-                "\r\n\r\n==============MENU==============");
-
-                Console.WriteLine("\r\nSelezionare il prodotto dal menù o digitare 11 per la stampa del conto:\r\n");
-                input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "1":
-                        conto += 2.50;
-                        break;
-                    case "2":
-                        conto += 5.20;
-                        break;
-                    case "3":
-                        conto += 10.00;
-                        break;
-                    case "4":
-                        conto += 12.50;
-                        break;
-                    case "5":
-                        conto += 3.50;
-                        break;
-                    case "6":
-                        conto += 8.00;
-                        break;
-                    case "7":
-                        conto += 5.00;
-                        break;
-                    case "8":
-                        conto += 5.00;
-                        break;
-                    case "9":
-                        conto += 6.00;
-                        break;
-                    case "10":
-                        conto += 7.90;
-                        break;
-                    case "11":
-                        Console.WriteLine($"\r\nIl conto totale (più servizio al tavolo di €3.00) è di €{(conto + 3):F2}");
-                        break;
-                    default:
-                        Console.WriteLine("\r\nScelta non valida! Riprova.\r\n");
-                        break;
-                }
-            } while (input != "11");
+                case "1":
+                    conto += 2.50;
+                    goto Start;
+                case "2":
+                    conto += 5.20;
+                    goto Start;
+                case "3":
+                    conto += 10.00;
+                    goto Start;
+                case "4":
+                    conto += 12.50;
+                    goto Start;
+                case "5":
+                    conto += 3.50;
+                    goto Start;
+                case "6":
+                    conto += 8.00;
+                    goto Start;
+                case "7":
+                    conto += 5.00;
+                    goto Start;
+                case "8":
+                    conto += 5.00;
+                    goto Start;
+                case "9":
+                    conto += 6.00;
+                    goto Start;
+                case "10":
+                    conto += 7.90;
+                    goto Start;
+                case "11":
+                    Console.WriteLine($"\r\nIl conto totale (più servizio al tavolo di €3.00) è di €{(conto + 3):F2}");
+                    break;
+                default:
+                    Console.WriteLine("\r\nScelta non valida! Riprova.\r\n");
+                    goto Start;
+            }
         }
     }
 }
